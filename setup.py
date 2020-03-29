7d887b0966c3e598bd87921bfc5ce577c9f43141
Gabriel Lourenço <gaabriiel.xd@gmail.com>
2020-03-29 10:58:09 -0300

    feat: remove distribute dependency

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ setup(
     platforms='any',
     test_suite='tests.alltests.suite',
     install_requires=[
-        'distribute',
+        # 'distribute',
         'reportlab'
     ],
     tests_require=[
